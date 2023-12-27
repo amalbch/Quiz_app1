cf6d5f682c42692b99e27feb3c799735b8cb2d1a
amalbch <amalbouchagraa@gmail.com>
2023-12-27 21:16:15 +0000

    commit1

---
+++ app/src/main/java/com/iset/quiz_app1/QuestionModel.java
@@ -0,0 +1,15 @@
+package com.iset.quiz_app1;
+
+
+
+
+        data class QuestionModel(val question:String,
+                                 val option1:String,
+                                 val option2:String,
+                                 val option3:String,
+                                 val option4:String,
+                                 val answer:String)
+)
+
+
+
